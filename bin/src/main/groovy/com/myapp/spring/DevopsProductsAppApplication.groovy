d5d32e37c0eb5dbc7b05d0023a6d53a835637b7a
poojashrivastav0829 <poojashri0829@gmail.com>
2023-03-10 15:24:11 +0530

    Jenkins git url is changed

---
+++ bin/src/main/groovy/com/myapp/spring/DevopsProductsAppApplication.groovy
@@ -0,0 +1,25 @@
+package com.myapp.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevopsProductsAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DevopsProductsAppApplication, args)
+	}
+
+}
+
+
+// SCM -> Git & Github
+
+// Build management -> Maven
+
+// Test Automation -> JUNIT
+
+// CI SERVER -> JENKINS
+
+
+// CONTINUOUS DELIVERY
---
+++ bin/src/main/groovy/com/myapp/spring/Test1.groovy
@@ -0,0 +1,56 @@
+package com.myapp.spring
+
+class Test1 {
+	
+	// Docker
+	// Docker Compose
+	// Docker Swarm
+	
+	
+	// docker container -d --name alpine-cont run alpine /bin/sh 
+	
+	// docker container ls -q or docker ps -a
+}
+
+
+// FROM python
+
+// RUN yum install -y wget || apt-get update && apt-get install -y wget
+
+// WORKDIR /app/bin
+
+// COPY . /app  || /web /app/web
+
+// ADD sample.tar.gz /app/bin/
+
+// CMD ["-c","5","127.0.0.1"]
+
+// ENTRYPOINT ["ping"]
+
+
+// FROM python
+
+// RUN mkdir -p /app
+
+// WORKDIR /app
+
+// COPY ./requirements.txt /app/
+
+// RUN pip install -r requirements.txt
+
+// ENTRYPOINT ["python"]
+
+// CMD ["main.py"]
+
+
+// RUN cd /app/bin
+// RUN touch sample.txt
+
+
+// WORKDIR /app/bin
+// RUN touch sample.txt 
+
+
+
+
+
